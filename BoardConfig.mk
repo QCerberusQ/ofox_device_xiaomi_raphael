d11e5c562908c90b70200bb27bd4e05d836880d9
me-cafebabe <me_cafebabe@hotmail.com>
2022-08-05 05:31:21 +0800

    raphael: Include logcat

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -77,3 +77,7 @@ TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel0-backlight/brightness"
 TW_MAX_BRIGHTNESS := 2047
 TW_THEME := portrait_hdpi
+
+# TWRP - Debug
+TARGET_USES_LOGD := true
+TWRP_INCLUDE_LOGCAT := true
