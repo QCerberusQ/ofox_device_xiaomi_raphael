9456b86659b8de3e37d4a6e74a764b2919b27e98
Helium-Studio <67852324+Helium-Studio@users.noreply.github.com>
2025-02-06 13:25:11 +0800

    raphael: Allow building with minimal manifest

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,3 +5,9 @@
 #
 
 DEVICE_PATH := device/xiaomi/raphael
+
+# Allow building with minimal manifest
+ALLOW_MISSING_DEPENDENCIES := true
+BUILD_BROKEN_DUP_RULES := true
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+BUILD_BROKEN_MISSING_REQUIRED_MODULES := true
