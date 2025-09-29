28453f1e1c16c9b18d689cf4cbdafdd239f9c0ca
Helium-Studio <67852324+Helium-Studio@users.noreply.github.com>
2025-09-29 20:21:13 +0800

    raphael: Convert to support dynamic partitions with FBEv2 encryption

--- device.mk
+++ device.mk
@@ -9,6 +9,12 @@ PRODUCT_PACKAGES += \
     qcom_decrypt \
     qcom_decrypt_fbe
 
+# Dynamic Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# Emulated storage
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
