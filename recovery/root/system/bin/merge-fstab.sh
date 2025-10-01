1c20cb60390773aa8eee1f104fbe2e0aa8413e3e
Helium-Studio <67852324+Helium-Studio@users.noreply.github.com>
2025-10-01 10:18:18 +0800

    raphael: Unify dynamic partitions and legacy partitions support

---
+++ recovery/root/system/bin/merge-fstab.sh
@@ -0,0 +1,18 @@
+#!/system/bin/sh
+
+if dd if=/dev/block/by-name/system bs=256k count=1 | strings | grep -q -E "raphael_dynamic_partitions|qti_dynamic_partitions" > /dev/null; then
+    echo >> /system/etc/recovery.fstab
+    for p in system system_ext product vendor odm; do
+        echo "${p} /${p} ext4 ro,barrier=1 wait,logical" >> /system/etc/recovery.fstab
+        echo "${p} /${p} erofs ro wait,logical" >> /system/etc/recovery.fstab
+    done
+    echo >> /system/etc/recovery.fstab
+    cat /system/etc/recovery.fstab.fbev2 >> /system/etc/recovery.fstab
+    echo >> /system/etc/twrp.flags
+    cat /system/etc/twrp.flags.dynamic >> /system/etc/twrp.flags
+else
+    echo >> /system/etc/recovery.fstab
+    cat /system/etc/recovery.fstab.fbev1 >> /system/etc/recovery.fstab
+    echo >> /system/etc/twrp.flags
+    cat /system/etc/twrp.flags.legacy >> /system/etc/twrp.flags
+fi
