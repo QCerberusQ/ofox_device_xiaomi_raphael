24608773d67a743578fa87cb466ff6d2513a82df
QCerberusQ <113352008+QCerberusQ@users.noreply.github.com>
2026-07-22 22:24:20 +0300

    raphael: Initial OFRP tree
    Updated device.mk to define hardware platform and API level, added recovery binaries and libraries, and set product property overrides.

--- device.mk
+++ device.mk
@@ -4,8 +4,11 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Assert
-TARGET_OTA_ASSERT_DEVICE := raphael,raphaelin
+# Hardware platform
+PRODUCT_PLATFORM := msmnile
+
+# API
+PRODUCT_SHIPPING_API_LEVEL := 28
 
 # Crypto
 PRODUCT_PACKAGES += \
@@ -15,9 +18,37 @@ PRODUCT_PACKAGES += \
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# Fastbootd (Mock implementation for recovery)
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.1-impl-mock
+
+# Additional binaries & libraries needed for recovery
+TARGET_RECOVERY_DEVICE_MODULES += \
+    libion \
+    libxml2 \
+    vendor.display.config@1.0 \
+    vendor.display.config@2.0 \
+    libdisplayconfig.qti
+
+TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so \
+    $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@1.0.so \
+    $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@2.0.so \
+    $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/libdisplayconfig.qti.so
+
+# Overrides
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=$(PRODUCT_RELEASE_NAME) \
+    TARGET_DEVICE=$(PRODUCT_RELEASE_NAME)
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.product.device=$(PRODUCT_RELEASE_NAME)
+
 # Emulated storage
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(LOCAL_PATH) \
+    vendor/qcom/opensource/commonsys-intf/display
